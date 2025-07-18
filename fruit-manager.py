23c72e98da32200c7fe23e8aa753c4f31928ec7b
Guillaume <guippon@gmail.com>
2025-07-18 16:09:13 +0200

    suppresion du commit d'origine

---
+++ fruit-manager.py
@@ -0,0 +1,29 @@
+inventaire = {
+    "bananes" : 120,
+    "mangues" : 85,
+    "ananas" : 45,
+    "noix de coco" : 60,
+    "papaye" : 30
+}
+
+def afficher_inventaire(inventaire:dict[str, int]):
+    print("Inventaire actuel de plantation :")
+    for fruit, quantite in inventaire.items():
+        print(f"- {fruit.capitalize()} : {quantite} unités" )
+        
+def recolter(inventaire:dict[str, int], fruit:str, quantite:int):
+    inventaire[fruit] = inventaire.get(fruit, 0) + quantite
+    print(f"\n✅ Récolté {quantite} {fruit} supplémentaires !")
+
+def vendre(inventaire, fruit, quantite):
+    if inventaire.get(fruit, 0) >= quantite:
+        inventaire[fruit] -= quantite
+        print(f"\n💰 Vendu {quantite} {fruit} !")
+    else:
+        print(f"\n❌ Pas assez de {fruit} pour vendre {quantite} {fruit} unités.")
+        
+if __name__ == "__main__":
+    afficher_inventaire(inventaire)
+    recolter(inventaire, "bananes", 10)
+    vendre(inventaire, "bananes", 5)
+    afficher_inventaire(inventaire)
