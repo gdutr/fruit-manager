2afe5cf47e6e6f86c0abdb15784f068bc5171e15
Guillaume <guippon@gmail.com>
2025-07-25 17:19:39 +0200

    ajout de la fonctionnalité tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -10,6 +10,17 @@ def ecrire_inventaire(inventaire, path="data/inventaire.json"):
     with open(path, 'w', encoding='utf-8') as fichier:
         json.dump(inventaire, fichier, ensure_ascii=False, indent=4)
 
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path, 'r', encoding='utf-8') as fichier:
+        tresorerie = json.load(fichier)
+    return tresorerie
+
+def ecrire_tresorerie(tresorerie,path="data/tresorerie.txt"):
+    with open(path, 'w', encoding='utf-8') as fichier:
+        json.dump(tresorerie, fichier, ensure_ascii=False, indent = 4)
+
+def afficher_tresorerie(tresorerie, path = "data/trésorerie.txt"):
+    print(f"\n💰 Trésorerie actuelle : {tresorerie:.2f} $")
 
 def afficher_inventaire(inventaire:dict[str, int]):
     print("Inventaire actuel de plantation :")
@@ -24,18 +35,24 @@ def recolter(inventaire:dict[str, int], fruit:str, quantite:int):
     
 
 
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorerie):
     if inventaire.get(fruit, 0) >= quantite:
         inventaire[fruit] -= quantite
-        print(f"\n💰 Vendu {quantite} {fruit} !")
+        tresorerie += 1 * quantite
+        print(f"\n💰 Vendu {quantite} {fruit} !")  
     else:
         print(f"\n❌ Pas assez de {fruit} pour vendre {quantite} {fruit} unités.")
+    return (inventaire, tresorerie)
 
         
 if __name__ == "__main__":
     inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
+    
     recolter(inventaire, "bananes", 10)
-    vendre(inventaire, "bananes", 5)
-    afficher_inventaire(inventaire)
+    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie)
+    
     ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
