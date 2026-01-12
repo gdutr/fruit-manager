23329e7628fedc640deba18e5b226320fdae477e
Guillaume <guippon@gmail.com>
2026-01-12 20:35:00 +0100

    suppression des derniers elements

--- pages/1_tresorerie.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from my_package.fruit_manager import *
-import requests
-import pandas as pd
-
-st.header("Bienvenue dans la section trésorerie")
-st.table(st.session_state.history)
